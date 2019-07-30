d77d89f201b3146e4d99dd7139724991c2dd67f8
rileypeterson <rileypeterson21@gmail.com>
2019-07-29 22:01:59 -0700

    Attempt to fix unfollowing...

--- depop_github.py
+++ depop_github.py
@@ -46,6 +46,7 @@ class Follower(object):
     LOGIN_URL = 'https://www.depop.com/login'
     MAX_USERS = 7499
     SUCCESSFUL_FOLLOW_MESSAGE = "Successfully followed {}! Have followed {} user(s)."
+    SUCCESSFUL_UNFOLLOW_MESSAGE = "Successfully unfollowed {}! Have unfollowed {} user(s)."
     def __init__(self, driver, home_user="", env='prod'):
         print("Starting Depop Follower...")
         self.driver = driver
@@ -220,7 +221,7 @@ class Follower(object):
                 driver.navigate().refresh()
                 self.click_elm(self._get_following_elm())
                 st = time.time()
-        print("Follower list loaded successfully")
+        print("Following list loaded successfully")
 
     def _get_unames_and_fol_btns(self, leading_ind, text="Follow"):
         name_elms = self.find_elements(driver.find_elements_by_xpath, """//p[contains(text(),'@')]""",
@@ -228,7 +229,7 @@ class Follower(object):
         # Rescope the follower list
         # self.click_elm(name_elms[-1])
         follow_btns_elms = self.find_elements(driver.find_elements_by_xpath, """//span[contains(text(),'{}')]""".format(text),
-                                              "Could not get follow button elms")[leading_ind:]
+                                              "Could not get {} button elms".format(text))[leading_ind:]
         follow_btns_elms = [e for e in follow_btns_elms if e.text == "{}".format(text)]
         unames = [e.text.replace('@', '') for e in name_elms]
         return unames, follow_btns_elms
@@ -256,7 +257,6 @@ class Follower(object):
         i = 1
         leading_ind = 1
         scroll_elm = self.find_element(driver.find_element_by_tag_name, 'html', "Couldn't find scroll_elm")
-
         unames, follow_btns_elms = self._get_unames_and_fol_btns(leading_ind)
         zipper = zip(unames, follow_btns_elms)
         while i < max_to_follow and (time.time() - st) < timeout:
@@ -287,35 +287,6 @@ class Follower(object):
             zipper = zip(unames, follow_btns_elms)
         unames, follow_btns_elms = self._get_unames_and_fol_btns(0)
         return unames
-                    # print("Successfully clicked on follower button")
-
-        # Don't try to follow users if we already followed them
-        # _user_names = [u for u in _user_names if u not in _followed_users]
-        # _user_elms = [e for e in driver.find_elements_by_xpath("""//span[contains(text(),'Follow')]""")[1:] if e.text == 'Follow']
-        # _user_elms = _user_elms[_num_followed:]
-        # for u, e in zip(_user_names, _user_elms):
-        #     print("Trying to follow: {}".format(u))
-        #     e.click()
-        #     print("Successfully followed: {}".format(u))
-        #     if env == 'dev':
-        #         # Close out of the both that comes up because I don't have an account
-        #         time.sleep(2)
-        #         # TODO: Fill bare exceptions
-        #         try:
-        #             driver.find_element_by_xpath("""//*[@id="mount"]/div/div/div[7]/div[2]/*""").click()
-        #         except:
-        #             try:
-        #                 driver.find_element_by_xpath("""/html/body/div[4]/div/div[2]/div/div/svg/path""").click()
-        #
-        #             except:
-        #                 pass
-        #         time.sleep(2)
-        #         # Refocus the scrolling
-        #     try:
-        #         driver.find_elements_by_xpath("""//p[contains(text(),'@')]""")[1:][0].click()
-        #     except:
-        #         print("Could not refocus scrolling element")
-        #     scroll_elm.send_keys(Keys.END)
 
     def follow(self, username):
         unames = [username]
@@ -342,9 +313,13 @@ class Follower(object):
         self.click_on_following_list()
         leading_ind = 1
         st = time.time()
+        print('Getting scroll element')
         scroll_elm = self.find_element(driver.find_element_by_tag_name, 'html', "Couldn't find scroll_elm")
-        unames, follow_btns_elms = self._get_unames_and_fol_btns(leading_ind, text="Following")
-        zipper = zip(unames, follow_btns_elms)
+        print('Getting usernames and following buttons')
+        unames, unfollowing_btns_elms = self._get_unames_and_fol_btns(leading_ind, text="Following")
+        print(unames, unfollowing_btns_elms)
+        assert len(unfollowing_btns_elms) > 0, "Couldn't find any unfollowing button elements :( ..."
+        zipper = zip(unames, unfollowing_btns_elms)
         while (time.time() - st) < timeout:
             for uname, unfol_btn in zipper:
                 print("Attempting to unfollow {}".format(uname))
@@ -354,7 +329,7 @@ class Follower(object):
                     leading_ind += 1
                     self.total_users_unfollowed += 1
                     self.unfollowed_users.append(uname)
-                    print(self.SUCCESSFUL_FOLLOW_MESSAGE.format(uname, self.total_users_followed))
+                    print(self.SUCCESSFUL_UNFOLLOW_MESSAGE.format(uname, self.total_users_unfollowed))
                 except Exception as e:
                     if str(e).startswith("Message: unknown error: Element <span>...</span> is not clickable at point"):
                         leading_ind += 1
@@ -364,6 +339,9 @@ class Follower(object):
                         raise e
                 time.sleep(random.random())
             scroll_elm.send_keys(Keys.END)
+            unames, unfollowing_btns_elms = self._get_unames_and_fol_btns(leading_ind, text="Following")
+            print(unames, unfollowing_btns_elms)
+            zipper = zip(unames, unfollowing_btns_elms)
 
 
 
@@ -379,116 +357,3 @@ if f.mode == 'following':
 elif f.mode == 'unfollowing':
     f.unfollow()
 
-# # Enter following mode
-# if mode=="following":
-#     # TODO: Try to just start following from either the users follower list or following list
-#
-#     # Click on the user's followers
-#     try:
-#         user_follower_elm = driver.find_element_by_xpath("""//span[contains(text(),'Followers')]""")\
-#             .find_element_by_xpath("..")
-#     except:
-#         raise FindingElementError("Failed to find follower button on a users page")
-#
-#     user_follower_num = int(user_follower_elm.text.replace(' Followers', ''))
-#
-#     # You're probably better off just stopping after a certain amount of time
-#     # Rather than a particular number of users
-#     break_time = 30
-#
-#     # Click on the users follower list
-#     try:
-#         user_follower_elm.click()
-#     except:
-#         raise ClickError("Failed to click on a list of followers")
-#     time.sleep(8)
-#
-#     # Start following
-#     scroll_elm = driver.find_element_by_tag_name('html')
-#     new_start_time = time.time()
-#     _num_followed = 0
-#     _followed_users = []
-#     while time.time() - new_start_time < 30:
-#         # Click on the follower name to bring the follower list into scope
-#         try:
-#             elms = driver.find_elements_by_xpath("""//p[contains(text(),'@')]""")[1:]
-#         except:
-#             raise FindingElementError("Failed to find users in the list (in order to return scope to scroll).")
-#
-#         try:
-#             elms[-1].click()
-#         except:
-#             raise ClickError("Failed to click the last user (in order to return scope to scroll).)")
-#
-#         _user_names = [u.text.replace('@', '') for u in driver.find_elements_by_xpath("""//p[contains(text(),'@')]""")]
-#
-#         # Don't try to follow users if we already followed them
-#         _user_names = [u for u in _user_names if u not in _followed_users]
-#         _user_elms = [e for e in driver.find_elements_by_xpath("""//span[contains(text(),'Follow')]""")[1:] if e.text == 'Follow']
-#         _user_elms = _user_elms[_num_followed:]
-#         for u, e in zip(_user_names, _user_elms):
-#             print("Trying to follow: {}".format(u))
-#             e.click()
-#             print("Successfully followed: {}".format(u))
-#             if env == 'dev':
-#                 # Close out of the both that comes up because I don't have an account
-#                 time.sleep(2)
-#                 # TODO: Fill bare exceptions
-#                 try:
-#                     driver.find_element_by_xpath("""//*[@id="mount"]/div/div/div[7]/div[2]/*""").click()
-#                 except:
-#                     try:
-#                         driver.find_element_by_xpath("""/html/body/div[4]/div/div[2]/div/div/svg/path""").click()
-#
-#                     except:
-#                         pass
-#                 time.sleep(2)
-#                 # Refocus the scrolling
-#             try:
-#                 driver.find_elements_by_xpath("""//p[contains(text(),'@')]""")[1:][0].click()
-#             except:
-#                 print("Could not refocus scrolling element")
-#             scroll_elm.send_keys(Keys.END)
-#
-#
-#
-#
-#
-# foo
-#
-#
-#
-# if mode=="unfollowing":
-#     prof_following_elm.click()
-#     time.sleep(3)
-#     scroll_elm = driver.find_element_by_tag_name('html')
-#     sauce=driver.page_source
-#     sauce=sauce.split()
-#     sauce=[i for i in sauce if "Following" in i and "span" in i][-1]
-#     data_word=sauce.split("=")[0]
-#     users_unfollowed=0
-#     reset_switch=0
-#     while users_unfollowed<prof_following_num:
-#         scroll_elm.send_keys(Keys.END)
-#         for user in driver.find_elements_by_css_selector("["+data_word+"='']"):
-#             try:
-#                 user.click()
-#                 users_unfollowed=users_unfollowed+1
-#                 reset_switch=reset_switch+1
-#             except:
-#                 pass
-#         if reset_switch>300:
-#             driver.refresh()
-#             prof_following_elm = driver.find_element_by_xpath("""//span[contains(text(),'Following')]""").find_element_by_xpath("..")
-#             prof_following_num = int(prof_following_elm.text.replace(' Following',''))
-#             prof_following_elm.click()
-#             time.sleep(3)
-#             users_unfollowed=0
-#             reset_switch=0
-#             scroll_elm = driver.find_element_by_tag_name('html')
-#     print("Done unfollowing.")
-
-
-if env == 'dev':
-    driver.close()
-        
