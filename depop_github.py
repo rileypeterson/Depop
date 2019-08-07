748d546e87274e058fa059886c9690d0c5ca755b
rileypeterson <rileypeterson21@gmail.com>
2019-08-07 00:22:11 -0700

    Update docstrings, refactoring, add todos, remove exceptions file

--- depop_github.py
+++ depop_github.py
@@ -1,27 +1,37 @@
+"""
+Program to follow and unfollow users for Depop.
+
+"""
+
 # Standard Library Imports
 import random
 import time
 import json
 
-
 # Third Party Imports
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-
-# Local Imports
-from depop_exceptions import *
 
 # User inputs
 chrome_path = r"your\path\to\chromedriver.exe"
 home_user="yourusernamehere"
 
 
-
 #####################################################################################
 
 
+# Exceptions
+def UserLandingPageError(Exception):
+    """ Failed to connect to the users page """
+    pass
 
+def ClickError(Exception):
+    """ Failed to click on an element """
+    pass
 
+def FindingElementError(Exception):
+    """ Failed to find an element """
+    pass
 
 # Store credentials (as dictionary) in config.json, if you want
 try:
@@ -38,7 +48,6 @@ env = config_json.get('env', 'prod')
 
 # driver should be global/reused
 driver = webdriver.Chrome(chrome_path)
-
 
 
 class Follower(object):
@@ -61,6 +70,18 @@ class Follower(object):
         self.on_follower_list = False
         self.on_following_list = False
         self.home_user_logged_in = False
+        self.current_username_num_followers = 0
+        self.current_username_num_following = 0
+        self.followed_users = []
+        self.unfollowed_users = []
+        self.window_size = None
+        self.set_env()
+
+    def set_env(self):
+        """
+        Set additional parameters in the init of the Follower class.
+
+        """
         if self.env == 'prod':
             self.mode = input("select mode (following, unfollowing):")
             assert self.mode == 'following' or self.mode == 'unfollowing', \
@@ -71,43 +92,85 @@ class Follower(object):
             else:
                 # Because we're going to unfollow...
                 self.current_username = self.home_user
-        else:
-            # I'm lazy
-            self.current_username = 'gsvwear'
-            self.mode = 'following'
-        self.current_username_num_followers = 0
-        self.current_username_num_following = 0
-        self.followed_users = []
-        self.unfollowed_users = []
-        self.window_size = None
+            return
+        # I'm lazy
+        self.current_username = 'gsvwear'
+        self.mode = 'following'
 
-    def set_on_attr(self, attr_to_set):
-        self.on_user_page = False
+    def reinitialize_on_user_page(self):
+        """
+        Reset attributes when navigating to the user page.
+
+        """
+        self.on_user_page = True
         self.on_follower_list = False
         self.on_following_list = False
-        setattr(self, attr_to_set, True)
 
-    def find_element(self, callable, arg, error=""):
+    def find_element(self, _callable, arg, error=""):
+        """
+        Find a single element. If incapable of being found, display a useful error message.
+
+        Parameters
+        ----------
+        _callable : callable
+            The driver method being used to find the element (e.g. driver.find_element_by_xpath).
+        arg : str
+            Pattern to be search for when finding the element.
+        error : str, optional
+            Error message (Selenium error messages are often not very obvious).
+
+        Returns
+        -------
+        selenium.webdriver.remote.webelement.WebElement
+
+        """
         if not error:
             error = "Failed to find an element with string arg {}".format(arg)
         try:
-            ret = callable(arg)
+            ret = _callable(arg)
         except Exception as e:
             print(error)
             raise e
         return ret
 
-    def find_elements(self, callable, arg, error=""):
+    def find_elements(self, _callable, arg, error=""):
+        """
+        Find a list of elements. If incapable of being found, display a useful error message.
+
+        Parameters
+        ----------
+        _callable : callable
+            The driver method being used to find the elements (e.g. driver.find_element_by_xpath).
+        arg : str
+            Pattern to be search for when finding the elements.
+        error : str, optional
+            Error message (Selenium error messages are often not very obvious).
+
+        Returns
+        -------
+        list [selenium.webdriver.remote.webelement.WebElement]
+
+        """
         if not error:
             error = "Failed to find elements with string arg {}".format(arg)
         try:
-            ret = callable(arg)
+            ret = _callable(arg)
         except Exception as e:
             print(error)
             raise FindingElementError(error)
         return ret
 
     def click_elm(self, elm, error=""):
+        """
+        Try to click and element. Display a meaningful error message if unsuccessful.
+        Parameters
+        ----------
+        elm : selenium.webdriver.remote.webelement.WebElement
+            The element to be clicked.
+        error : str, optional
+            The meaningful error message to display.
+
+        """
         if not error:
             error = "Failed to click element with text {}".format(elm.text)
         try:
@@ -117,12 +180,24 @@ class Follower(object):
             raise e
 
     def nav_to_user(self, username=None):
+        """
+        Navigate to a user's page.
+        Parameters
+        ----------
+        username : str, optional
+            Username of the user to navigate to.
+
+        """
         if not username:
             username = self.current_username
         self.driver.get(self.BASE_URL + username)
-        self.set_on_attr('on_user_page')
+        self.reinitialize_on_user_page()
 
-    def nav_to_mainpage(self):
+    def nav_to_login_page(self):
+        """
+        Navigate to the login page so the user can login with their credentials.
+
+        """
         # Make the driver and navigate to the depop main page.
         if env == 'prod':
             # If in production we should login
@@ -131,11 +206,17 @@ class Follower(object):
             self.home_user_logged_in = True
 
     def _get_following_elm(self):
+        """
+        Return the element that can be clicked to bring up the list of users that a user is following.
+        Returns
+        -------
+        selenium.webdriver.remote.webelement.WebElement
+
+        """
         following_elm_child = self.find_element(self.driver.find_element_by_xpath,
                                            """//span[contains(text(),'Following')]""",
                                            error="Couldn't find Following element child")
-        # Have to do this :shrug:
-        # TODO: Fix. You should enable method on obj that isn't driver
+        # TODO: Must be a better way
         try:
             following_elm_parent = following_elm_child.find_element_by_xpath("..")
         except Exception as e:
@@ -143,12 +224,19 @@ class Follower(object):
             raise e
         return following_elm_parent
 
+    # TODO: Refactor. Combine with above definition
     def _get_follower_elm(self):
+        """
+        Return the element that can be clicked to bring up the list of users that are following a user (followers).
+        Returns
+        -------
+        selenium.webdriver.remote.webelement.WebElement
+
+        """
         follower_elm_child = self.find_element(self.driver.find_element_by_xpath,
                                            """//span[contains(text(),'Followers')]""",
                                            error="Couldn't find Follower element child")
-        # Have to do this :shrug:
-        # TODO: Fix. You should enable method on obj that isn't driver
+        # TODO: Must be a better way.
         try:
             follower_elm_parent = follower_elm_child.find_element_by_xpath("..")
         except Exception as e:
@@ -157,13 +245,30 @@ class Follower(object):
         return follower_elm_parent
 
     def print_elm_attrs(self, elm):
+        """
+        Print out attributes of element.
+        Parameters
+        ----------
+        elm : selenium.webdriver.remote.webelement.WebElement
+
+        """
         print("Location: ", elm.location,
               "Size: ", elm.size,
               "Text: ", elm.text,
               "Is selected:", elm.is_selected(),
               "Is enabled:", elm.is_enabled())
 
+
+    # TODO: These next few sections are pretty meaty. Could use a refactor.
     def get_user_stats(self, username=None):
+        """
+        Obtain/set particular stats about a specific user (how many they follow, how many followers).
+        Parameters
+        ----------
+        username : str
+            Username to get stats for.
+
+        """
         if not username:
             username = self.current_username
         self.nav_to_user(username)
@@ -186,9 +291,15 @@ class Follower(object):
                                                 self.current_username_num_followers))
 
     def click_on_follower_list(self):
+        """
+        Try (several times) to bring up on screen the list of followers.
+
+        """
         self.click_elm(self._get_follower_elm())
         elms = []
         total_time = time.time()
+        # TODO: Refactor. There are better ways to implement a timeout... you can use the timeout elsewhere
+        # This could be much simpler... disappointed in myself
         st = time.time()
         while (time.time() - st) < 20:
             while not elms:
@@ -205,6 +316,11 @@ class Follower(object):
         print("Follower list loaded successfully")
 
     def click_on_following_list(self):
+        """
+        Try (several times) to bring up on screen the list of following users.
+
+        """
+        # TODO: Refactor. Combine with above and generally make better.
         self.click_elm(self._get_following_elm())
         elms = []
         total_time = time.time()
@@ -217,13 +333,33 @@ class Follower(object):
                 break
             else:
                 if (time.time() - total_time) > 100:
-                    raise TimeoutError("Failed to click on follower list")
+                    raise TimeoutError("Failed to click on following list")
                 driver.navigate().refresh()
                 self.click_elm(self._get_following_elm())
                 st = time.time()
         print("Following list loaded successfully")
 
     def _get_unames_and_fol_btns(self, leading_ind, text="Follow"):
+        """
+        With the list of followers/following open, return the usernames and clickable follow/following elements
+        which are within the scrollable view.
+        Parameters
+        ----------
+        leading_ind : int
+            The list of elements will include all the elements in the scrollable section, the leading index
+            is used to slice this to exclude elements already followed/unfollowed.
+        text : str, optional
+            The text ('Follow', 'Following') which is located in the element which can be clicked to follow/unfolllow
+            a user.
+
+        Returns
+        -------
+        unames: list [str]
+            The list of usernames.
+        follow_btn_elms: list [selenium.webdriver.remote.webelement.WebElement]
+            The list of follow/unfollow button elements.
+
+        """
         name_elms = self.find_elements(driver.find_elements_by_xpath, """//p[contains(text(),'@')]""",
                                        "Could not get name elms")[leading_ind:]
         # Rescope the follower list
@@ -234,7 +370,18 @@ class Follower(object):
         unames = [e.text.replace('@', '') for e in name_elms]
         return unames, follow_btns_elms
 
+    # TODO: Should be private...
     def handle_not_login_error(self, fol_btn, uname):
+        """
+        Handle the error the occurs when a user (me) tries to use this program without being logged in.
+        Parameters
+        ----------
+        fol_btn : selenium.webdriver.remote.webelement.WebElement
+            The 'Follow' element which invokes the not being logged in popup.
+        uname : str
+            Username.
+
+        """
         print("Trying to click out of alert")
         try:
             action = webdriver.common.action_chains.ActionChains(self.driver)
@@ -251,6 +398,8 @@ class Follower(object):
         except Exception as e:
             raise e
 
+    # TODO: These last ones are so ugly it's not work writing docstrings for
+    # URGENT
     def follow_from_list(self, max_num=50, timeout=60):
         max_to_follow = min(max_num, self.current_username_num_followers)
         st = time.time()
@@ -320,7 +469,7 @@ class Follower(object):
         print(unames, unfollowing_btns_elms)
         assert len(unfollowing_btns_elms) > 0, "Couldn't find any unfollowing button elements :( ..."
         zipper = zip(unames, unfollowing_btns_elms)
-        while (time.time() - st) < timeout:
+        while (time.time() - st) < timeout and self.home_user_num_following > 3:
             for uname, unfol_btn in zipper:
                 print("Attempting to unfollow {}".format(uname))
                 print("Attempting click on unfollower button")
@@ -329,6 +478,8 @@ class Follower(object):
                     leading_ind += 1
                     self.total_users_unfollowed += 1
                     self.unfollowed_users.append(uname)
+                    self.home_user_num_following -= 1
+                    print(self.home_user_num_following)
                     print(self.SUCCESSFUL_UNFOLLOW_MESSAGE.format(uname, self.total_users_unfollowed))
                 except Exception as e:
                     if str(e).startswith("Message: unknown error: Element <span>...</span> is not clickable at point"):
@@ -346,14 +497,27 @@ class Follower(object):
 
 
 
+if __name__ == '__main__':
+    # Init
+    f = Follower(driver, home_user, env)
 
-f = Follower(driver, home_user, env)
-# Login
-f.nav_to_mainpage()
-# Go to home user and get their stats
-f.get_user_stats(f.home_user)
-if f.mode == 'following':
-    f.follow(f.current_username)
-elif f.mode == 'unfollowing':
-    f.unfollow()
+    # Login
+    f.nav_to_login_page()
+
+    # Go to home user and get their stats
+    f.get_user_stats(f.home_user)
+
+    if f.mode == 'following':
+        f.follow(f.current_username)
+
+    # Think this should work... can't test though
+    # Unfollow users for 60 seconds, refresh and repeat until down to 3 users
+    elif f.mode == 'unfollowing':
+        while f.home_user_num_following > 3:
+            f.unfollow()
+            print('Pausing for 3 seconds')
+            time.sleep(3)
+            f.get_user_stats(f.home_user)
+            print('Pausing for 3 seconds')
+            time.sleep(3)
 
